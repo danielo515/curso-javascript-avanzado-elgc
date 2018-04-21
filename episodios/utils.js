85348d444196d834644ac881d0756ebd4dbd4af2
Danielo Rodriguez <rdanielo@gmail.com>
2018-04-21 13:12:39 +0200

    feat: improved utils

--- episodios/utils.js
+++ episodios/utils.js
@@ -1,5 +1,21 @@
 'use strict';
-const rand = () => Math.random() * 100 | 0
+const {range, random: rand} = require('lodash');
+const {name, date, commerce, random, address} = require('faker');
+
+const languages = ['Java', 'C', 'C++', 'C#', 'Python', 'Visual Basic .NET', 'PHP', 'JavaScript', 'Pascal', 'Swift', 'Perl', 'Ruby', 'Assembly language', 'R', 'Visual Basic', 'Objective-C', 'Go', 'MATLAB', 'PL/SQL', 'Scratch']
+
+const makePeople = (n) => range(n).map(
+    () => ({
+        name: name.firstName(),
+        job: name.jobType(),
+        age: rand(18,71),
+        startedAt: date.past().getFullYear(),
+        workingAt: address.country(),
+        likings: {
+            color: commerce.color(),
+            programmingLanguage: random.arrayElement(languages)
+        }
+    }));
 
 
-module.exports = { rand }
+module.exports = { rand, makePeople }
