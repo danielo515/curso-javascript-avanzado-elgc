a2478f02e4129b2970670d521832f1d223cb30d2
Danielo Rodriguez <rdanielo@gmail.com>
2018-05-03 21:16:52 +0200

    feat(episodes): added episode 05

---
+++ episodios/05-inspect/index.js
@@ -0,0 +1,64 @@
+'use strict';
+const { sample, random } = require('lodash');
+// -_-_-_-_- REPL SESSION STARTS HERE -_-_-_-_-
+const people = [
+  [
+    {
+      name: 'Enid',
+      job: 'Supervisor',
+      age: 39,
+      startedAt: 2017,
+      workingAt: 'Holy See (Vatican City State)',
+      likings: { color: 'sky blue', programmingLanguage: 'Pascal' }
+    }
+  ],
+  [
+    {
+      name: 'Adeline',
+      job: 'Orchestrator',
+      age: 53,
+      startedAt: 2018,
+      workingAt: 'Sao Tome and Principe',
+      likings: { color: 'white', programmingLanguage: 'Visual Basic' }
+    },
+    {
+      name: 'Lessie',
+      job: 'Developer',
+      age: 45,
+      startedAt: 2018,
+      workingAt: 'Poland',
+      likings: { color: 'silver', programmingLanguage: 'PL/SQL' }
+    }
+  ],
+  [
+    {
+      name: 'Lavonne',
+      job: 'Developer',
+      age: 58,
+      startedAt: 2018,
+      workingAt: 'Belize',
+      likings: { color: 'indigo', programmingLanguage: 'Scratch' }
+    },
+    {
+      name: 'Darian',
+      job: 'Officer',
+      age: 43,
+      startedAt: 2018,
+      workingAt: 'Gambia',
+      likings: { color: 'turquoise', programmingLanguage: 'Assembly language' }
+    },
+    {
+      name: 'Nichole',
+      job: 'Supervisor',
+      age: 19,
+      startedAt: 2018,
+      workingAt: 'Saudi Arabia',
+      likings: { color: 'ivory', programmingLanguage: 'Javascript' },
+    }
+  ]
+];
+const family = people.map( group => group.map( person => {
+    const randGroup = people[random(0,people.length -1)]
+    const randPerson = randGroup[random(0,randGroup.length -1)]
+    return Object.assign(person, {sibling: randPerson})
+}));
